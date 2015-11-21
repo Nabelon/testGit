fa14f686a21b7db8a52e5ac7c8ca6457c7c00bbb
Ben2 <haho.hoha@web.de>
2015-11-21 13:46:13 +0100

    test ignore

---
+++ ToViET/main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+#include "gui.h"
+#include "fileexplorer.h"
+#include "comparisondialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    FileExplorer w;
+    w.show();
+    GUI g;
+    g.show();
+    comparisonDialog c;
+    c.show();
+
+    return a.exec();
+}
